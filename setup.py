be47da5df5f834f27d75f7025073764e9f779243
Rich <richardleelovestoprogram@gmail.com>
2024-05-18 06:44:04 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import find_packages,setup
+from typing import List
 
-
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function returns a list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements[req.replace("\n","")for req in requirements]
 
 setup(
     name='mlproject',

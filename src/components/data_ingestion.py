f8102e70bb6d49e797ad755318733298cd7f0509
Rich <richardleelovestoprogram@gmail.com>
2024-06-03 05:44:38 -0400

    update data_ingestion.py

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import Logger
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+

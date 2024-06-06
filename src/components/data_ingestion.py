727b5d82ddd6ec8bcf4716b20f09aafc0e283d39
Rich <richardleelovestoprogram@gmail.com>
2024-06-06 06:42:37 -0400

    update data ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from src.exception import CustomException
-from src.logger import Logger
+from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -12,3 +12,15 @@ class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts', "train.csv")
     test_data_path: str=os.path.join('artifacts', "test.csv")
     raw_data_path: str=os.path.join('artifacts', "data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+        except:
+            pass

f71c739eca7c15365aa02233477906ce99ee95f3
Rich <richardleelovestoprogram@gmail.com>
2024-06-09 05:53:22 -0400

    update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -24,5 +24,10 @@ class DataIngestion:
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            logging.info("Train test split initiated")
+            
         except:
             pass

7d23f1cc5c5c0fd87c15f788d5b31ef777cf9ecb
Rich <richardleelovestoprogram@gmail.com>
2024-06-10 04:53:59 -0400

    update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,6 +28,8 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             
             logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
             
         except:
             pass

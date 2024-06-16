afd67689bbc67a3dceb1737e0d8af970047c0dd7
Rich <richardleelovestoprogram@gmail.com>
2024-06-16 06:41:28 -0400

    update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -30,20 +30,20 @@ class DataIngestion:
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to.csv(self.ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
-            test_set.to.csv(self.ingestion_config.test_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
             logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
 
             )
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__ == "__main__":
-    obj=DataIngestion
+if __name__=="__main__":
+    obj=DataIngestion()
     obj.initiate_data_ingestion()

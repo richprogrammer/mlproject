d962999dc66ecb81d4185a706b249cfef148cb6a
Rich <richardleelovestoprogram@gmail.com>
2024-06-21 06:20:39 -0400

    update data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,3 +6,5 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+

579b7ef036d86340a642f40dd5a649f4751790fa
Rich <richardleelovestoprogram@gmail.com>
2024-07-15 08:15:03 -0400

    update data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -86,6 +86,7 @@ class DataTransformation:
                 target_column_name="math_score"
                 numerical_columns = ["writing_score", "reading_score"]
 
-                input_feature_train_df=train_df[target_column_name]
+                input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+                target_feature_train_df=train_df[target_column_name]
             except:
                 pass

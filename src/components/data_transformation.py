e7a6e83082b84884582730167d690680daa7516a
Rich <richardleelovestoprogram@gmail.com>
2024-07-16 09:19:41 -0400

    update data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -88,5 +88,8 @@ class DataTransformation:
 
                 input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
                 target_feature_train_df=train_df[target_column_name]
+
+                input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+                target_feature_test_df=test_df[target_column_name]
             except:
                 pass

ae20a9de6c0bbe832f7afe9039388e95a2e8d861
Rich <richardleelovestoprogram@gmail.com>
2024-07-17 08:04:19 -0400

    update data transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -91,5 +91,9 @@ class DataTransformation:
 
                 input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
                 target_feature_test_df=test_df[target_column_name]
+
+                logging.info(
+                    f"Applying preprocessing object on training dataframe and testing dataframe"
+                    )
             except:
                 pass

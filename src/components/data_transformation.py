792c8fe6bd4c596ad40469dc57e6707795e17b15
Rich <richardleelovestoprogram@gmail.com>
2024-07-20 10:18:16 -0400

    update data_transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -97,5 +97,6 @@ class DataTransformation:
                     )
                 
                 input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+                input_feature_test_arr=preprocessing_obj.transform(input_feature_train_df)
             except:
                 pass

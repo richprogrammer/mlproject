1f04b74ded0b963bb3fef7d7776a9dd05db1b1f5
Rich <richardleelovestoprogram@gmail.com>
2024-08-03 09:01:17 -0400

    update data_ingestion.py

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -52,3 +52,4 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformatio()

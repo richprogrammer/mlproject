82781d6ec7f490a7b164381802e3add0e5372d2b
Rich <richardleelovestoprogram@gmail.com>
2024-08-05 06:12:11 -0400

    update data_transforamtion.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -120,5 +120,5 @@ class DataTransformation:
                     test_arr,
                     self.data_transformation_config.preprocessor_obj_file_path,
                 )
-            except:
-                pass
+            except Exception as e:
+                raise CustomException(e,sys)

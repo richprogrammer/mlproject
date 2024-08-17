8e6514d8db864c744f1328fdaaae2f97a05829e3
Rich <richardleelovestoprogram@gmail.com>
2024-08-17 07:57:07 -0400

    update model_trainer.py

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,6 +49,16 @@ class ModelTrainer:
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,x_test=X_test,y_test=y_test,
                                              models=models)
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
 
+            ## To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+
+            
         except:
             pass

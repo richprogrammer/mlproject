54a088df64b845f4fb52a09419f35a6c269d5ddb
Rich <richardleelovestoprogram@gmail.com>
2024-09-06 21:38:08 -0400

    update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -28,7 +29,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            model.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
             #model.fit(X_train, y_train)  # Train model
 
